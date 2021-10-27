3ec17cb3d711e4146fffb7842130f8e36313f8ee
ENGO150 <v.smejkal06@gmail.com>
2021-10-27 16:08:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ENGODATABASE
+
+This is easy to use database system. 
+
+Download it with `git  clone https://github.com/ENGO150/EngoDatabase.git` and it's ready!
+
+Use void readString from edb.h (in this file is also explained how it works).
---
+++ include/edb.h
@@ -0,0 +1,12 @@
+/*
+    This is header file for edb.c
+*/
+
+#ifndef CLEARTEMP_EDB_H
+#define CLEARTEMP_EDB_H
+
+#include <stdio.h>
+
+void readString(char objectRaw[], FILE * edbLoc, char ** objectFinal); //This reads objectRaw (string) from edbLoc and stores it into objectFinal
+
+#endif
